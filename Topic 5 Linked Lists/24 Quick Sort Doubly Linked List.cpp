ca99b65c0eb2d57af126673f421e5777ec63c880
Tejas Singh Bedi <tejas.bedi1@gmail.com>
2020-04-20 23:53:21 +0530

    Doubly List, Quick Sort

--- Topic 5 Linked Lists/24 Quick Sort Doubly Linked List.cpp
+++ Topic 5 Linked Lists/24 Quick Sort Doubly Linked List.cpp
@@ -53,42 +53,162 @@ void display(node *head)
 	debug (1, "\n");
 }
 
+void display(node *first, node *last)
+{
+	node *head = first;
+	while (head != last)
+	{
+		debug (1, head->data, " ");
+		head = head->next;
+	}
+	debug (1, head->data, ", ");
+	
+	while (head != first)
+	{
+		debug (1, head->data, " ");
+		head = head->prev;
+	}
+	debug (1, head->data, "\n");
+}
+
 
 node *sortPartition(node **first, node**last)
 {
 	node *f = *first;
 	node *l = *last;
-	debug(2, "Sorting Partition: first, last: ", f->data, " ", l->data, "\n");
+	debug(2, "Sorting Partition: BEGIN first, last: ", f->data, " ", l->data, "\n");
 	
 	node *p = *last; 	// Partition
 	node *cont = *first; // Control
 	node *temp;
 	
-	while (cont != l)
+	// This loop moves all nodes bigger than the partition to the back (of the partition)
+	while (cont != p)
 	{
+		debug (3, "Sorting Partition: Analysing cont: ", cont->data, "\n");
+		
 		if (p->data > cont->data)
 		{
 			cont = cont->next;
 		}
 		else
 		{
-			
+			if (cont == f)
+			{
+				if (cont->prev != NULL)
+				{
+					temp = cont->next;
+					
+					cont->prev->next = cont->next;
+					cont->next->prev = cont->prev;
+					
+					if (l->next != NULL)
+					{
+						cont->next = l->next;
+						l->next->prev = cont;
+						
+						cont->prev = l;
+						l->next = cont;
+						l = cont;
+					}
+					else
+					{
+						cont->next = NULL;
+						cont->prev = l;
+						
+						l->next = cont;
+						l = cont;
+					}
+					
+					cont = temp;
+					f = cont;
+				}
+				else
+				{
+					temp = cont->next;
+					cont->next->prev = NULL;
+					
+					if (l->next != NULL)
+					{
+						cont->next = l->next;
+						l->next->prev = cont;
+						
+						cont->prev = l;
+						l->next = cont;
+						l = cont;
+					}
+					else
+					{
+						cont->next = NULL;
+						cont->prev = l;
+						
+						l->next = cont;
+						l = cont;
+					}
+					
+					cont = temp;
+					f = cont;
+				}
+			}
+			else
+			{
+				temp = cont->next;
+					
+				cont->prev->next = cont->next;
+				cont->next->prev = cont->prev;
+				
+				if (l->next != NULL)
+				{
+					cont->next = l->next;
+					l->next->prev = cont;
+					
+					cont->prev = l;
+					l->next = cont;
+					l = cont;
+				}
+				else
+				{
+					cont->next = NULL;
+					cont->prev = l;
+					
+					l->next = cont;
+					l = cont;
+				}
+				
+				cont = temp;
+					
+			}
 		}
+		
+		debug(3, "Sorting Partition: After loop f, l: ", f->data, " ", l->data, "\n");
 	}
+	
+	*first = f;
+	*last = l;
+	debug(2, "Sorting Partition: END first, partition, last: ", f->data, " ", p->data, " ", l->data, "\n");
+	if (LOG_LEVEL >= 3) display(f, l);
 	
 	return p;
 }
 
+
 void qSort(node **f, node **l)
 {
-	debug (2, "QSort: First & Last: ", f->data, ",", l->data, "\n");
+	debug (2, "\nQSort: First & Last: ", (*f)->data, ",", (*l)->data, "\n");
 	node *p;
 	
 	if ( (*f) != (*l) )
 	{
 		p = sortPartition( &(*f), &(*l) );
-		sortPartition( &(*f), &(p->prev) );
-		sortPartition( &(p->next), &(*l) );
+		
+		debug(2, "QSort: first, partition, last: ", (*f)->data, " ", (p)->data, " ", (*l)->data, "\n");
+		if (p->prev != NULL && *f != p && *f != p->prev)
+			qSort( &(*f), &(p->prev) );
+		
+		debug(2, "QSort: first, partition, last: ", (*f)->data, " ", (p)->data, " ", (*l)->data, "\n");
+	
+		if (p->next != NULL && *l != p && *l != p->next)
+			qSort( &(p->next), &(*l) );
 	}
 }
 
@@ -104,7 +224,11 @@ void quickSort(node **head)
 
 int main ()
 {
+	// Okay this one i didnt write very neatly but ..
+	// Its fucking 3 on a saturday and I wannaa sleep
+	
 	// Resource
+	// https://www.geeksforgeeks.org/quick-sort/
 	
 	LOG_LEVEL = 2;
 
@@ -112,9 +236,9 @@ int main ()
 	
 	debug (2, "Pushing data...\n");
 
-	push(&head,3);
-	push(&head,8);
 	push(&head,1);
+	push(&head,8);
+	push(&head,3);
 	push(&head,9);
 	push(&head,5);
 	push(&head,7);
